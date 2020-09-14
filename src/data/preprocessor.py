3da5e8dcb062080bb19dfb9a3b71074dd14e551a
iKiYo <kiyotoikuma2017@gmail.com>
2020-09-14 10:35:33 +0200

    feat: added train/test csv split function

--- src/data/preprocessor.py
+++ src/data/preprocessor.py
@@ -1,11 +1,14 @@
-import pandas as pd 
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 data_file_name="2012-2013-data-with-predictions-4-final.csv"
 data_folder_path = "/content/drive/My Drive/master thesis/Datasets/assistment_dataset/"
 prepared_data_name="assist12_4cols_noNaNskill.csv"
 
-data_file_path=data_folder_path+data_file_name
+data_file_path=data_folder_path + data_file_name
 output_path = data_folder_path + prepared_data_name
 
   
@@ -50,3 +53,27 @@ def preprocess_csv(csv_file_path=data_file_path, out_path=output_path):
   print(F"preprocessed file is exported to \n{out_path}\n")
 
 
+def make_train_test_csv(data_folder_path, csv_dataname, test_ratio):
+  """Split a given csv dataset into train/test csv file
+
+  Input:  path of full dataset CSV file in the  format below
+                 <format>students id, skill id, correct, x(skill_id*2+1)
+  Output: None, directory store train/test csv file in the same folder of input
+  """
+  # Read and use preprocessed csv file
+  df = pd.read_csv(os.path.join(data_folder_path, csv_dataname))
+
+  # Get N, M, T
+  num_students = df['user_id'].nunique()
+  num_skills = df['skill_id'].nunique()
+  max_sequence_length=  df['user_id'].value_counts().max()
+  print(F"number of students:{num_students}  number of skills:{num_skills}  max attempt :{max_sequence_length}")
+
+  # Split whole data into train/test data
+  train_index, test_index = train_test_split(np.arange(num_students), test_size=test_ratio, random_state=42)
+  train_df = df[df['user_id'].isin(train_index)]
+  test_df = df[df['user_id'].isin(test_index)]
+
+  # Write them out to the same folder
+  train_df.to_csv(os.path.join(data_folder_path,"train_"+csv_dataname), index=False)
+  test_df.to_csv(os.path.join(data_folder_path,"test_"+csv_dataname), index=False)
