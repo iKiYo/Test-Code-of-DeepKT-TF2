e7df27acb279deb43a1746d9651f4d5382b1e57a
iKiYo <kiyotoikuma2017@gmail.com>
2020-12-02 01:24:54 +0200

    Feat: added original dktforget 2 model

--- src/data/tempo_tf_data_preprocessor.py
+++ src/data/tempo_tf_data_preprocessor.py
@@ -14,7 +14,6 @@ from sklearn.model_selection import KFold
 preprocessed_csv_path = "../../data/processed/assist12_4cols_noNaNskill.csv"
 batch_size = 25
 
-
 def get_kfold_id_generator(array, num_fold):
   kf = KFold(n_splits = num_fold, shuffle = True, random_state = 2)
   return  kf.split(array)
@@ -63,6 +62,27 @@ def make_sequence_data(data_folder_path, processed_csv_dataname):
 
   return seq
 
+
+def make_dkt_forget_2_seq(data_folder_path, processed_csv_dataname):
+
+  df = pd.read_csv(os.path.join(data_folder_path, processed_csv_dataname))
+
+    # Get N, M, T
+  num_students = df['user_id'].nunique()
+  num_skills = df['skill_id'].nunique()
+  max_sequence_length=  df['user_id'].value_counts().max()
+  print(F"number of students:{num_students}  number of skills:{num_skills}  max attempt :{max_sequence_length}")
+
+  seq = df.groupby('user_id').apply(
+      lambda r: (
+          r['x'].values[:-1], # x
+          r['seq_delta_t'].values[1:], # delta_t
+          r['skill_id'].values[:-1], # q_t
+          r['correct'].values[1:], # a_t
+      )
+  )
+  assert num_students, len(seq)
+  return seq
 
 def prepare_cv_id_array(data_folder_path, train_csv_dataname, num_fold):#, *hparams, *num_hparam_search):
   """ Make index array for X th fold cross validation splitting train/validation dataset
@@ -157,6 +177,64 @@ def prepare_batched_tf_data(preprocessed_csv_seq, batch_size, num_skills, max_se
   return dict_dataset  
 
 
+def prepare_batched_tf_data_2(preprocessed_csv_seq, batch_size, num_skills, max_sequence_length):
+ 
+  seq = preprocessed_csv_seq 
+  
+  # Transform into tf.data format
+  dataset = tf.data.Dataset.from_generator(
+      generator=lambda: seq,
+      output_types=(
+                    tf.int32, # x
+                    tf.float32, # delta current
+                    tf.int32, # q target skill
+                    tf.int32) # a response
+  )
+
+  # One hot enconding
+  # Encode binary sign of attmpts(from M to 2M)
+  transformed_dataset  = dataset.map(
+      lambda x, delta, q, label: (
+          tf.one_hot(x, depth=num_skills*2), # x
+        # tf.tile(tf.expand_dims(delta, -1), tf.constant([1,num_skills], tf.int32)), # expanded_delta
+          tf.expand_dims(delta, -1),  # delta
+          # tf.one_hot(q, depth=num_skills, on_value=1.0, off_value=0.0, axis=-1), # current skill positive(for counts)
+          tf.one_hot(q, depth=num_skills, axis=-1), # q 
+          tf.expand_dims(label, -1) # a 
+      )
+  )
+
+  # Padding for LSTM
+  # FIX: padding value should be Args and default -1
+  padded_dataset = transformed_dataset.padded_batch(
+          batch_size=batch_size,
+          padding_values=(.0, .0, .0, -1),
+          padded_shapes=(
+                          [max_sequence_length, 2*num_skills],  # x          
+                          # [max_sequence_length, num_skills], # expanded_delta
+                          [max_sequence_length, 1], # delta
+                          # [max_sequence_length, num_skills], # positive sign of skill
+                          [max_sequence_length, num_skills], # q (skill of next step attempt)
+                          [max_sequence_length, 1], # a
+                          ),
+          drop_remainder=True
+  )
+
+  # Dict format dataset to feed built-in function such as model.fit
+  dict_dataset = padded_dataset.map(
+          lambda x, delta, q, a : (
+              {"x": x,
+              "q" : q,
+              "delta" : delta,
+              #  "expanded_delta" : expanded_delta,
+              #  "skill" : skill
+                },
+              { "outputs" : a}
+          )
+  )
+  return dict_dataset  
+
+
 def split_dataset(dataset, total_size, test_fraction, val_fraction=None):
     def split(dataset, split_size):
         split_set = dataset.take(split_size)
@@ -184,4 +262,3 @@ def split_dataset(dataset, total_size, test_fraction, val_fraction=None):
         train_set, val_set = split(train_set, val_size)
 
     return train_set, test_set, val_set
-
