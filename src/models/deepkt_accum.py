79591485a46705a25d2362b6b7e49c0004325abc
iKiYo <kiyotoikuma2017@gmail.com>
2021-03-03 12:41:49 +0900

    feat: constant learning curve method

--- src/models/deepkt_accum.py
+++ src/models/deepkt_accum.py
@@ -32,9 +32,16 @@ class DKTAccumModel(tf.keras.Model):
     count_mask = layers.Masking(mask_value=0, input_shape=(None, 2*num_skills))
     count_cell = CountStateRNNCell(num_skills*2)
     c_count = layers.RNN(count_cell, return_sequences=True)
-    c_emb =  layers.Dense(embed_dim)
+    # c_emb =  layers.Dense(embed_dim)
+    c_emb =  layers.TimeDistributed(layers.Dense(num_skills*2, activation='relu',
+    kernel_constraint=tf.keras.constraints.MinMaxNorm(
+    min_value=0.0, max_value=1.0, rate=1.0, axis=0
+)))
+# tf.keras.constraints.NonNeg()))
+    # c_coef =  layers.TimeDistributed(layers.Dense(embed_dim))
     # time
     delta_emb = layers.TimeDistributed(layers.Dense(1))
+    delta_coeff = layers.TimeDistributed(layers.Dense(1))
 
     # combine x and _c_t
     c_dot = layers.Multiply()
@@ -56,14 +63,44 @@ class DKTAccumModel(tf.keras.Model):
     # count data
     count_t =count_mask(x) # 2M
     count_t = c_count(count_t) # accmulates
-    count_t = tf.math.log1p(count_t)
-    embed_count = c_emb(count_t) # 2M to N-1
+    # *** learning curve method 
+    # *** log form 
+    # count_t = tf.math.log1p(count_t)
+    # embed_count = c_emb(count_t) # 2M to N-1
 
-    # forgetting curve
+    # *** BEST learning constant learning curve 
+    embed_count = c_emb(count_t) # 2M to N-1
+    exp_count= tf.ones(shape=(tf.shape(embed_count))) - tf.math.exp(-embed_count)
+    # coef_count = c_coef(exp_count)
+
+    # *** forgetting curve
+    # *** preprocess for pure Pareto (log(1+raw t))
+    # pre_delta = tf.math.log1p(delta)
+    # embed_delta=delta_emb(pre_delta)
+    # embed_delta=delta_emb(delta)
+    # exp_delta= tf.math.exp(-embed_delta)
+
+    # *** preprocess for coeff Pareto (log(1+mu*raw t))
+    # coef_delta = delta_coeff(delta)
+    # pre_delta = tf.math.log1p(coef_delta)
+    # embed_delta=delta_emb(pre_delta)
+    # embed_delta=delta_emb(delta)
+    # exp_delta= tf.math.exp(-embed_delta)
+
+    # *** BEST for log2_Parteto (log2(1+ t))preprocessed)
     embed_delta=delta_emb(delta)
     exp_delta= tf.math.exp(-embed_delta)
 
-    x_c_t = c_concat([embed_x, embed_count, exp_delta]) # N+N+1
+    # for Expo (mu*expo(-alpha*delta))
+    # embed_delta=delta_emb(delta)
+    # exp_delta= tf.math.exp(-embed_delta)
+    # embed_exp_delta = delta_coeff(exp_delta)
+
+    # *** concatenations
+    # x_c_t = c_concat([embed_x, embed_count, exp_delta]) # N+N+1
+    x_c_t = c_concat([embed_x, exp_count, exp_delta]) # N+N+1
+    # x_c_t = c_concat([embed_x, coef_count, exp_delta]) # N+N+1
+    # x_c_t = c_concat([embed_x, embed_count, embed_exp_delta]) # N+N+1
     # x_c_t = c_concat([embed_count, exp_delta]) # N + 1 No x Cout + Delta_t
     # x_c_t = c_concat([embed_x, embed_count, embed_delta]) # N+N+1
 
